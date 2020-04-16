1c3188617cfdbfbcfee0452ec8885c15733f71e1
Akagha Chinaka <akaghachinaka@gmail.com>
2020-04-16 17:37:31 +0100

    initial commit
    initial commit

---
+++ COOLAPI/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COOLAPI.Models;
+
+namespace COOLAPI.Controllers
+{   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private StudentContext _studentContext;
+
+        public StudentController(StudentContext context)
+        {
+            _studentContext = context;
+        }
+
+        ~StudentController()
+        {
+            _studentContext.Dispose();
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            return _studentContext.Students.ToList();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
